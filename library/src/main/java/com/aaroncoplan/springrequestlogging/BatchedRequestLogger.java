4ab62dda89b24d1dae317bb80d4bfb9f6aaad62f
AaronCoplan <afcoplan@gmail.com>
2019-03-21 18:46:23 -0400

    Defining interfaces for the two types of request logger.

---
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/BatchedRequestLogger.java
@@ -0,0 +1,10 @@
+package com.aaroncoplan.springrequestlogging;
+
+import java.util.List;
+
+public interface BatchedRequestLogger {
+
+    int getBatchSize();
+    void processRequestBatch(List<RequestData> batch);
+    long getMaxBatchAgeMS(); // avoid having batch sit there forever during periods of inactivity
+}
---
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/SingleRequestLogger.java
@@ -0,0 +1,6 @@
+package com.aaroncoplan.springrequestlogging;
+
+public interface SingleRequestLogger {
+
+    void processSingleRequest(RequestData requestData);
+}
