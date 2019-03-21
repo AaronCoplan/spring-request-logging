597f9bca669b88eb8d2f21c870457bb81445e2d2
AaronCoplan <afcoplan@gmail.com>
2019-03-21 18:09:25 -0400

    Fixed dependencies.

---
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/RequestLogger.java
@@ -0,0 +1,7 @@
+package com.aaroncoplan.springrequestlogging;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RequestLogger extends HandlerInterceptorAdapter {
+
+}
