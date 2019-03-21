0caf22e837335bb2b689a95a66ac2b7889a771a6
AaronCoplan <afcoplan@gmail.com>
2019-03-21 18:21:14 -0400

    Began implementing request logger and using in sample-app.

--- library/src/main/java/com/aaroncoplan/springrequestlogging/RequestLogger.java
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/RequestLogger.java
@@ -1,7 +1,26 @@
 package com.aaroncoplan.springrequestlogging;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class RequestLogger extends HandlerInterceptorAdapter {
 
+    private final String TIMING_ATTRIBUTE = "REQUEST_START_TIME_MILLIS";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute(TIMING_ATTRIBUTE, System.currentTimeMillis());
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        final long completionTime = System.currentTimeMillis();
+        final long startTime = (long)request.getAttribute(TIMING_ATTRIBUTE);
+        System.out.println(completionTime-startTime);
+        super.afterCompletion(request, response, handler, ex);
+    }
 }
--- sample-application/src/main/java/com/aaroncoplan/Application.java
+++ sample-application/src/main/java/com/aaroncoplan/Application.java
@@ -1,16 +1,24 @@
 package com.aaroncoplan;
 
+import com.aaroncoplan.springrequestlogging.RequestLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application implements WebMvcConfigurer {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestLogger());
     }
 
     @GetMapping("/")
