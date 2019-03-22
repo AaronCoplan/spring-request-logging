1a299e28fa647b01959886ef57c8dae2ff45b1b5
AaronCoplan <afcoplan@gmail.com>
2019-03-21 21:38:35 -0400

    Updated RequestLoggerFactory so it compiles.

--- library/src/main/java/com/aaroncoplan/springrequestlogging/RequestLoggerFactory.java
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/RequestLoggerFactory.java
@@ -3,10 +3,10 @@ package com.aaroncoplan.springrequestlogging;
 public class RequestLoggerFactory {
 
     public static SingleRequestInterceptor buildSingleRequestLogger(SingleRequestLogger singleRequestLogger) {
-
+        return new SingleRequestInterceptor(singleRequestLogger);
     }
 
     public static BatchedRequestInterceptor buildBatchedRequestLogger(BatchedRequestLogger batchedRequestLogger) {
-
+        return new BatchedRequestInterceptor();
     }
 }
