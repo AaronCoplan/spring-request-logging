145cc51c464150196dc424d0dce6b4b23c3f62c1
AaronCoplan <afcoplan@gmail.com>
2019-03-21 22:05:38 -0400

    Implemented BatchedRequestInterceptor with batching logic.

--- library/src/main/java/com/aaroncoplan/springrequestlogging/BatchedRequestInterceptor.java
+++ library/src/main/java/com/aaroncoplan/springrequestlogging/BatchedRequestInterceptor.java
@@ -1,12 +1,62 @@
 package com.aaroncoplan.springrequestlogging;
 
-public class BatchedRequestInterceptor {
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BatchedRequestInterceptor extends HandlerInterceptorAdapter {
 
     private final BatchedRequestLogger batchedRequestLogger;
+    private final Object lock;
+    private RequestData[] batchData;
+    private int index;
 
     public BatchedRequestInterceptor(BatchedRequestLogger batchedRequestLogger) {
         this.batchedRequestLogger = batchedRequestLogger;
+        this.batchData = new RequestData[batchedRequestLogger.getBatchSize()];
+        this.lock = new Object();
+        this.index = 0;
     }
 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        RequestUtils.applyTimingStart(request);
+        return super.preHandle(request, response, handler);
+    }
 
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        RequestData requestData = RequestUtils.extractRequestData(request, response);
+
+        List<RequestData> batchToProcess = null;
+        synchronized (lock) {
+            System.out.println(Thread.currentThread().getId() + " Entering CS");
+
+            batchData[index] = requestData;
+            ++index;
+            if(index == batchedRequestLogger.getBatchSize()) {
+                // process the batch and reinit the array
+                // this allows us to exit the CS faster
+                // we absolutely don't want to process the batch in the CS
+                batchToProcess = Arrays.asList(batchData);
+                batchData = new RequestData[batchedRequestLogger.getBatchSize()];
+            }
+
+            System.out.println(Thread.currentThread().getId() + " Exiting CS");
+        }
+
+        // check if there is a batch to process
+        // if there is, call the process function
+        if(batchToProcess != null) {
+            batchedRequestLogger.processRequestBatch(batchToProcess);
+        }
+
+        super.afterCompletion(request, response, handler, ex);
+    }
 }
